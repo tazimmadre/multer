a10d266989dd616289bd7f3439ba09c6889795cd
Tazim_Git <tazimmadre5041@gmail.com>
2021-11-23 20:02:10 +0530

    images

--- src/List.js
+++ src/List.js
@@ -20,7 +20,13 @@ export class List extends React.Component{
       return (
         <ul className="list-group" key={x._id}>
           <li className="list-group-item d-flex justify-content-between align-items-center">
-            image
+            <img
+              src={x.profileImg}
+              width="100px"
+              height="100px"
+              alt={x._id}
+              className="img-thumbnail"
+            />
             <a
               className="badge bg-primary rounded-pill"
               download
@@ -28,6 +34,17 @@ export class List extends React.Component{
             >
               <button type="button" className="btn btn-primary">
                 Download
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-download"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
+                  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
+                </svg>
               </button>
             </a>
           </li>
@@ -37,7 +54,27 @@ export class List extends React.Component{
     render(){
       return (
         <div className="container">
-{this.renderedlist()}
+          <div className="header ">
+            <Link to="/">
+              <button className="btn btn-primary">
+                Upload
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-upload"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
+                  <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708l3-3z" />
+                </svg>
+              </button>
+            </Link>
+
+          </div>
+          <br/>
+          {this.renderedlist()}
         </div>
       );
     }
--- src/files-upload-component.js
+++ src/files-upload-component.js
@@ -1,61 +1,91 @@
-import React, { Component } from "react";
+import React, {useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router";
+import {Link} from 'react-router-dom';
 
-export default class FilesUploadComponent extends Component {
-  constructor(props) {
-    super(props);
-    
-    this.onFileChange = this.onFileChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-    
-    this.state = {
-      profileImg: "",
-      flag:false
-    };
+const FilesUploadComponent=()=> {
+    let navigate=useNavigate();
+    const [state,setState]=useState({      
+         profileImg: "",
+         flag:false,
+         redirect:"/list"
+    });
+
+  
+  const onFileChange=(e) =>{
+    setState({ profileImg: e.target.files[0] });
   }
   
-  onFileChange(e) {
-    this.setState({ profileImg: e.target.files[0] });
-  }
-  
-  onSubmit(e) {
+  const onSubmit=(e)=> {
     e.preventDefault();
     const formData = new FormData();
-    formData.append("profileImg", this.state.profileImg);
+    formData.append("profileImg", state.profileImg);
     axios
       .post("http://localhost:4000/api/user-profile", formData, {})
       .then((res) => {
-        this.setState({flag:true});
+        setState({flag:true});
         console.log(res);
+        navigate("/list");
       });
 
   }
-  render() {
-    if(!this.state.flag){
-        return (
-        <div className="container">
-          <div className="row">
-            <form onSubmit={this.onSubmit}>
-              <div className="form-group">
-                <input type="file" onChange={this.onFileChange} />
-              </div>
-              <div className="form-group">
-                <button className="btn btn-primary" type="submit">
-                  Upload
-                </button>
-              </div>
-            </form>
+  return (
+    <div>
+      <figure>
+        <blockquote className="blockquote">
+          <p>A site where you can upload images</p>
+        </blockquote>
+        <figcaption className="blockquote-footer">
+          made with multer and reactJs
+        </figcaption>
+      </figure>
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-md">
+            <Link className="btn btn-primary stretched-link" to="/list">
+              Uploads
+              <br />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-upload"
+                viewBox="0 0 16 16"
+              >
+                <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
+                <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708l3-3z" />
+              </svg>
+            </Link>
+            <p>To get all Uploads click on Uploads</p>
           </div>
-        </div>
-        );
-      }else if(this.state.flag){
-        return (
-          <div className="d-flex justify-content-center">
-            <div className="spinner-border" role="status">
-              <span className="visually-hidden">Loading...</span>
-            </div>
+        </nav>
+      </div>
+      <br />
+      <div className="container">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input type="file" onChange={onFileChange} />
           </div>
-        );
-      }
-  }
+          <div className="form-group">
+            <button className="btn btn-primary" type="submit">
+              Upload
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-upload"
+                viewBox="0 0 16 16"
+              >
+                <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
+                <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708l3-3z" />
+              </svg>
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
+export default FilesUploadComponent;
