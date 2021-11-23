4d4de095c6829664adc8da0cda320a3e057b4a78
Tazim <tazimmadre5041@gmail.com>
2021-11-23 14:20:58 +0530

    multerfile-upload

---
+++ backend/database/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+  db: 'mongo "mongodb://cluster0-shard-00-00.oxjfe.mongodb.net:27017,cluster0-shard-00-01.oxjfe.mongodb.net:27017,cluster0-shard-00-02.oxjfe.mongodb.net:27017/myFirstDatabase?replicaSet=atlas-toiw5s-shard-0" --ssl --authenticationDatabase admin --username Tazim --password qwertyuiop1234567890',
+};
